563989d807bed4b6f9edd85fc9b47b7a7446ad87
Ivannia Gomez <82829719+DarthIV02@users.noreply.github.com>
2025-01-30 22:12:02 -0800

    Update 2 pins

--- led.c
+++ led.c
@@ -1,23 +1,26 @@
 #include <wiringPi.h>
 #include <stdio.h>
-#define LEDPin 0
-#define LEDPin 1
+#define LEDPinRed 0
+#define LEDPinYellow 1
 
 int main(void)
 {
- if(wiringPiSetup() == -1){
- //if the wiringPi initialization fails, print error message
- printf("setup wiringPi failed !");
- return 1;
- }
- pinMode(LEDPin, OUTPUT);
- while(1){
- digitalWrite(LEDPin, LOW); //led off
- printf("led off...\n");
- delay(500);
- digitalWrite(LEDPin, HIGH); //led on
- printf("...led on\n");
- delay(500);
- }
- return 0;
+    if(wiringPiSetup() == -1){
+    //if the wiringPi initialization fails, print error message
+        printf("setup wiringPi failed !");
+        return 1;
+    }
+    pinMode(LEDPinRed, OUTPUT);
+    pinMode(LEDPinYellow, OUTPUT);
+    while(1){
+        digitalWrite(LEDPinRed, LOW); //led red off
+        digitalWrite(LEDPinYellow, HIGH); //led yellow on
+        printf("led off...\n");
+        delay(500);
+        digitalWrite(LEDPinRed, HIGH); //led red on
+        digitalWrite(LEDPinYellow, LOW); //led yellow off
+        printf("...led on\n");
+        delay(500);
+    }
+    return 0;
 }
