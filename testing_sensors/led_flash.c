85f2c8e920cc884efe870a3ee6c0d7c3d9713c14
Ivannia Gomez <82829719+DarthIV02@users.noreply.github.com>
2025-01-30 22:34:33 -0800

    flash_led

---
+++ testing_sensors/led_flash.c
@@ -0,0 +1,23 @@
+#include <wiringPi.h>
+#include <stdio.h>
+#define LEDPin 3
+
+int main(void)
+{
+    if(wiringPiSetup() == -1){
+    //if the wiringPi initialization fails, print error message
+        printf("setup wiringPi failed !");
+        return 1;
+    }
+    pinMode(LEDPin, OUTPUT);
+    
+    while(1){
+        digitalWrite(LEDPin, HIGH); //led on
+        printf("led on...\n");
+        delay(4000);
+        digitalWrite(LEDPin, LOW); //led off
+        printf("...led off\n");
+        delay(2000);
+    }
+    return 0;
+}
