dd17c3571d9e29844c0214e9f2db905a559e2a45
Ivannia Gomez <82829719+DarthIV02@users.noreply.github.com>
2025-01-31 18:48:23 -0800

    Update buzzer.c

--- testing_sensors/buzzer.c
+++ testing_sensors/buzzer.c
@@ -26,14 +26,58 @@ int main(void) {
     pinMode(BUZZER, OUTPUT);
 
     // Melody notes
-    int melody[] = {NOTE_C5, NOTE_D5, NOTE_E5, NOTE_F5, NOTE_G5, NOTE_A5, NOTE_B5, NOTE_C6};
-    int duration = 500;  // Each note duration in milliseconds
+    int melody[] = {
+        NOTE_AS4, NOTE_AS4, NOTE_AS4,
+        NOTE_F5, NOTE_C6,
+        NOTE_AS5, NOTE_A5, NOTE_G5, NOTE_F6, NOTE_C6,
+        NOTE_AS5, NOTE_A5, NOTE_G5, NOTE_F6, NOTE_C6,
+        NOTE_AS5, NOTE_A5, NOTE_AS5, NOTE_G5, NOTE_C5, NOTE_C5, NOTE_C5,
+        NOTE_F5, NOTE_C6,
+        NOTE_AS5, NOTE_A5, NOTE_G5, NOTE_F6, NOTE_C6,
+
+        NOTE_AS5, NOTE_A5, NOTE_G5, NOTE_F6, NOTE_C6,
+        NOTE_AS5, NOTE_A5, NOTE_AS5, NOTE_G5, NOTE_C5, NOTE_C5,
+        NOTE_D5, NOTE_D5, NOTE_AS5, NOTE_A5, NOTE_G5, NOTE_F5,
+        NOTE_F5, NOTE_G5, NOTE_A5, NOTE_G5, NOTE_D5, NOTE_E5, NOTE_C5, NOTE_C5,
+        NOTE_D5, NOTE_D5, NOTE_AS5, NOTE_A5, NOTE_G5, NOTE_F5,
+
+        NOTE_C6, NOTE_G5, NOTE_G5, REST, NOTE_C5,
+        NOTE_D5, NOTE_D5, NOTE_AS5, NOTE_A5, NOTE_G5, NOTE_F5,
+        NOTE_F5, NOTE_G5, NOTE_A5, NOTE_G5, NOTE_D5, NOTE_E5, NOTE_C6, NOTE_C6,
+        NOTE_F6, NOTE_DS6, NOTE_CS6, NOTE_C6, NOTE_AS5, NOTE_GS5, NOTE_G5, NOTE_F5,
+        NOTE_C6
+    };
+    int durations[] = {
+        8, 8, 8,
+        2, 2,
+        8, 8, 8, 2, 4,
+        8, 8, 8, 2, 4,
+        8, 8, 8, 2, 8, 8, 8,
+        2, 2,
+        8, 8, 8, 2, 4,
+
+        8, 8, 8, 2, 4,
+        8, 8, 8, 2, 8, 16,
+        4, 8, 8, 8, 8, 8,
+        8, 8, 8, 4, 8, 4, 8, 16,
+        4, 8, 8, 8, 8, 8,
+
+        8, 16, 2, 8, 8,
+        4, 8, 8, 8, 8, 8,
+        8, 8, 8, 4, 8, 4, 8, 16,
+        4, 8, 4, 8, 4, 8, 4, 8,
+        1
+    };
+
+    int size = sizeof(durations) / sizeof(int);
 
     while (1) {
-        for (int thisNote = 0; thisNote < 8; thisNote++) {
+        for (int thisNote = 0; thisNote < size; thisNote++) {
             printf("Playing frequency: %d Hz\n", melody[thisNote]);
+            int duration = 1000 / durations[thisNote];
             playTone(melody[thisNote], duration);
-            delay(50);  // Small gap between notes
+            int pauseBetweenNotes = duration * 1.30;
+            delay(pauseBetweenNotes); // Small gap between notes
         }
         delay(2000);  // Pause before repeating
     }
