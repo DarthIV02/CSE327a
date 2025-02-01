9e08f410e22cd45ae02dcb732677ff913a1bbd3d
Ivannia Gomez <82829719+DarthIV02@users.noreply.github.com>
2025-01-31 17:52:48 -0800

    Update rotary.c

--- testing_sensors/rotary.c
+++ testing_sensors/rotary.c
@@ -33,6 +33,7 @@ int main(void) {
     while(1){
         // Read the current state of CLK
         currentStateCLK = digitalRead(CLK);
+        printf("Clock: ", currentStateCLK)
 
         // If last and current state of CLK are different, then pulse occurred
         // React to only 1 state change to avoid double count
@@ -72,7 +73,7 @@ int main(void) {
             lastButtonPress = millis();
         }
 
-        delay(25);
+        delay(1);
     }
 
     return 0;
