7b03dafd8f3990247db5ef98598f83b022bf5bea
Ivannia Gomez <82829719+DarthIV02@users.noreply.github.com>
2025-02-01 17:55:58 -0800

    Section 1 - Og files

---
+++ section1/assignment1.c
@@ -0,0 +1,52 @@
+#include "assignment1.h"
+#include <stdio.h>
+#include <wiringPi.h>
+#include <softPwm.h>
+
+void init_shared_variable(SharedVariable* sv) {
+    sv->bProgramExit = 0;
+// You can initialize the shared variable if needed.
+}
+
+void ledInit(void) {
+    softPwmCreate(PIN_SMD_RED, 0, 0xff);
+//......
+//initialize SMD and DIP
+}
+
+void init_sensors(SharedVariable* sv) {
+// .......
+    ledInit();
+}
+
+// 1. Button
+void body_button(SharedVariable* sv) {
+}
+
+// 2. Infrared Motion Sensor
+void body_motion(SharedVariable* sv) {
+}
+
+// 3. Microphone Sound Sensor
+void body_sound(SharedVariable* sv) {
+}
+
+// 4. Rotary Encoder
+void body_encoder(SharedVariable* sv) {
+}
+
+// 5. DIP two-color LED
+void body_twocolor(SharedVariable* sv) {
+}
+
+// 6. SMD RGB LED
+void body_rgbcolor(SharedVariable* sv) {
+}
+
+// 7. Auto-flash LED
+void body_aled(SharedVariable* sv) {
+}
+
+// 8. Buzzer
+void body_buzzer(SharedVariable* sv) {
+}
