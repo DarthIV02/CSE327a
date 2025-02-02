8767f53aaa6e77e54e2bad5a2b2479c6cf85fd7f
Ivannia Gomez <82829719+DarthIV02@users.noreply.github.com>
2025-02-01 17:53:13 -0800

    Update led_off.c

--- testing_sensors/led_off.c
+++ testing_sensors/led_off.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #define LEDPinRed 8
 #define LEDPinYellow 9
+#define LEDPFlash 12
 
 int main(void)
 {
@@ -12,9 +13,11 @@ int main(void)
     }
     pinMode(LEDPinRed, OUTPUT);
     pinMode(LEDPinYellow, OUTPUT);
+    pinMode(LEDPFlash, OUTPUT);
     
     digitalWrite(LEDPinRed, LOW); //led red off
     digitalWrite(LEDPinYellow, LOW); //led yellow off
+    digitalWrite(LEDPFlash, LOW); //led flash off
     
     return 0;
 }
