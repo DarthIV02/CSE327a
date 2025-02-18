45b8743e9c752dbade723f4e1a560948e20bfe9e
Ivannia Gomez <82829719+DarthIV02@users.noreply.github.com>
2025-02-17 17:54:13 -0800

    Update main_section2.c

--- section2_2/main_section2.c
+++ section2_2/main_section2.c
@@ -102,10 +102,10 @@ int main(int argc, char* argv[]) {
 
 	finish_workload();
 
-	//printDBG("Total idle time: %llu\n", sv.total_idle_time);
-	//printDBG("Total high time: %llu\n", sv.total_high_time);
-	//printDBG("Total low time: %llu\n", sv.total_low_time);
-	//printDBG("Total energy: %llu\n", ((sv.total_idle_time * 50) + (sv.total_low_time * 1200) + (sv.total_high_time * 2000)) / 1000000);
+	printDBG("Total idle time: %llu\n", sv.total_idle_time);
+	printDBG("Total high time: %llu\n", sv.total_high_time);
+	printDBG("Total low time: %llu\n", sv.total_low_time);
+	printDBG("Total energy: %llu\n", ((sv.total_idle_time * 50) + (sv.total_low_time * 1200) + (sv.total_high_time * 2000)) / 1000000);
 
 	release_buffer(fileno(stdout));
 	printf("Scheduler program finished.\n");
