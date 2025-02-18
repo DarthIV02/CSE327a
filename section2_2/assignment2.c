0b7bdd6ea211c3f36f12444d2dc981b8a6c4167f
Ivannia Gomez <82829719+DarthIV02@users.noreply.github.com>
2025-02-17 19:24:41 -0800

    Laxity?

--- section2_2/assignment2.c
+++ section2_2/assignment2.c
@@ -167,12 +167,19 @@ TaskSelection select_task(SharedVariable* sv, const int* aliveTasks, long long i
 				prev_selection = act_idx;
 			}
 
+			if (prev_selection != act_idx){
+				if (workloadDeadlines[act_idx] - sv->workloadExecution_ind[act_idx] < workloadDeadlines[prev_selection] - sv->workloadExecution_ind[prev_selection]){
+					prev_selection = act_idx;
+				}
+			}
+
 			long long closest_deadline = workloadDeadlines[act_idx];
 			if (prev_selection == act_idx){
 				pred_time += sv->workloadExecution_ind[act_idx + 8]; //Slowest it can run
 			} else {
 				pred_time += sv->workloadExecution_ind[act_idx]; //Fastest it can run
 			}
+
 			if((time % closest_deadline) + pred_time > closest_deadline){ //Past and previous deadline
 				prev_freq = 1; //Run it fast
 				if (prev_selection != -1){
@@ -181,9 +188,9 @@ TaskSelection select_task(SharedVariable* sv, const int* aliveTasks, long long i
 			}
 		}
 
-		if (prev_selection == -1){
+		/*if (prev_selection == -1){
 			pred_time += sv->workloadExecution_ind[act_idx]; //Fastest it can run
-		}
+		}*/
 	}
 
 	/*for (int i = 0; i < NUM_TASKS; ++i) {
