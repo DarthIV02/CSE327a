c930ac24f59219d7797025f06d1d79dbaab436a8
Ivannia Gomez <82829719+DarthIV02@users.noreply.github.com>
2025-02-20 12:44:12 -0800

    Update calls.py

--- project_v1/calls.py
+++ project_v1/calls.py
@@ -16,9 +16,13 @@ def call_for_help(channel):
 CARETAKER_NUMBER = os.getenv("CARETAKER_NUMBER", "") # your relative numbers comma-separated
 print("Calling Phone Number: ", CARETAKER_NUMBER)
 ORGINATE_NUMBER = os.getenv("TWILIO_NUMBER", "") # should be one of your Twilio active number
+print("From number: ", ORGINATE_NUMBER)
 PATIENT = os.getenv("PATIENT_NAME", "") # should be one of your Twilio active number
 HELP_TEXT = f"Help call from {PATIENT} !"
+print("Sending message: ", HELP_TEXT)
 # Find your Account SID and Auth Token at twilio.com/console
 # and set the environment variables. See http://twil.io/secure
 ACCOUNT_SID = os.environ.get('TWILIO_ACCOUNT_SID', "")
 AUTH_TOKEN = os.environ.get('TWILIO_AUTH_TOKEN', "")
+
+call_for_help()
