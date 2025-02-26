83563c7096f5abcb55e1f7cba3a1a7f358941d8a
Ivannia Gomez <82829719+DarthIV02@users.noreply.github.com>
2025-02-26 13:27:24 -0800

    Update multiple_button_window.c

--- project_v1/multiple_button_window.c
+++ project_v1/multiple_button_window.c
@@ -36,8 +36,7 @@ static void activate(GtkApplication *app, void *user_data)
 
 int main(int argc, char *argv[])
 {
-  g_autoptr(GtkApplication) app = gtk_application_new(NULL, 
-  G_APPLICATION_FLAGS_NONE);
+  g_autoptr(GtkApplication) app = gtk_application_new(NULL, G_APPLICATION_DEFAULT_FLAGS);
   g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
 
   return g_application_run(G_APPLICATION(app), argc, argv);
