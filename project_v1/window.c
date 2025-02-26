53ca0508e9745d2ad2b47c5d8e5aa7909761cfb9
Ivannia Gomez <82829719+DarthIV02@users.noreply.github.com>
2025-02-26 13:44:41 -0800

    Update window.c

--- project_v1/window.c
+++ project_v1/window.c
@@ -6,7 +6,7 @@ activate (GtkApplication* app,
 {
   GtkWidget *window = gtk_application_window_new(app);
   GtkWidget *grid   = gtk_grid_new();
-  GtkWidget *label  = gtk_label_new("6:59");
+  GtkWidget *label  = gtk_label_new("6:59 pm");
 
   gtk_window_set_title (GTK_WINDOW (window), "Elder Care System");
   gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER); // Center the window
@@ -15,22 +15,18 @@ activate (GtkApplication* app,
   gtk_grid_set_column_spacing(GTK_GRID(grid),10);
   gtk_grid_set_row_spacing(GTK_GRID(grid), 6);
   // Create a new button
-  GtkWidget *button1 = gtk_button_new_with_label ("Press 1");
-  GtkWidget *button2 = gtk_button_new_with_label ("Press 2");
-  GtkWidget *button3 = gtk_button_new_with_label ("Press 3");
+  GtkWidget *button1 = gtk_button_new_with_label ("Patient Details");
+  GtkWidget *button2 = gtk_button_new_with_label ("Schedule");
 
   gtk_grid_attach(GTK_GRID(grid), label, 0, 0, 3, 1);
   gtk_grid_attach(GTK_GRID(grid), button1, 0, 1, 1, 1);
   gtk_grid_attach(GTK_GRID(grid), button2, 1, 1, 1, 1);
-  gtk_grid_attach(GTK_GRID(grid), button3, 2, 1, 1, 1);
 
   gtk_container_add(GTK_CONTAINER(window), grid);
   // When the button is clicked, close the window passed as an argument
   g_signal_connect_swapped (button1, "clicked", G_CALLBACK (gtk_window_close), 
   window);
   g_signal_connect_swapped (button2, "clicked", G_CALLBACK (gtk_window_close), 
-  window);
-  g_signal_connect_swapped (button3, "clicked", G_CALLBACK (gtk_window_close),
   window);
 
   gtk_widget_show_all(window);
