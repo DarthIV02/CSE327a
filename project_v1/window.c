79fa055e60111dc655607fd75571037954293515
Ivannia Gomez <82829719+DarthIV02@users.noreply.github.com>
2025-02-26 13:41:53 -0800

    Update window.c

--- project_v1/window.c
+++ project_v1/window.c
@@ -2,7 +2,7 @@
 
 static void
 activate (GtkApplication* app,
-          gpointer        user_data)
+          void        user_data)
 {
   GtkWidget *window = gtk_application_window_new(app);
   GtkWidget *grid   = gtk_grid_new();
@@ -42,13 +42,8 @@ activate (GtkApplication* app,
 int
 main (int argc, char **argv)
 {
-  GtkApplication *app;
-  int status;
+  g_autoptr(GtkApplication) app = gtk_application_new(NULL, G_APPLICATION_DEFAULT_FLAGS);
+  g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
 
-  app = gtk_application_new ("org.gtk.example", G_APPLICATION_DEFAULT_FLAGS);
-  g_signal_connect (app, "activate", G_CALLBACK (activate), NULL);
-  status = g_application_run (G_APPLICATION (app), argc, argv);
-  g_object_unref (app);
-
-  return status;
+  return g_application_run(G_APPLICATION(app), argc, argv);
 }
