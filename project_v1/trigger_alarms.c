b256c8b806a72a81b7d21119e5bdb1bcaa8413dc
Ivannia Gomez <82829719+DarthIV02@users.noreply.github.com>
2025-02-27 01:24:08 -0800

    Update trigger_alarms.c

--- project_v1/trigger_alarms.c
+++ project_v1/trigger_alarms.c
@@ -162,11 +162,11 @@ int main() {
             for(int i = 0; i < num_medicine; i++){
                 Medicine med = medicines[i];
                 DateTime temp;
-                date_add_hours(&time_struct, med->repeat_hour);
-                date_add_minutes(&time_struct, med->repeat_minute);
+                date_add_hours(&time_struct, &med->repeat_hour);
+                date_add_minutes(&time_struct, &med->repeat_minute);
                 char buffer[80];
                 strftime(buffer, 80, "%Y-%m-%d %H:%M", &time_struct); 
-                parse_complete(buffer, temp->year, temp->month, temp->day, temp->hour)
+                parse_complete(buffer, &temp->year, &temp->month, &temp->day, &temp->hour)
                 if (temp->day == current_dt->day){
                     medicine_active[i] = 1;
                 } else {
