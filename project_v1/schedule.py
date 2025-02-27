a4e296fe4edd284f39da6ab776e55d836208f1ce
Ivannia Gomez <82829719+DarthIV02@users.noreply.github.com>
2025-02-26 20:42:42 -0800

    Update schedule.py

--- project_v1/schedule.py
+++ project_v1/schedule.py
@@ -11,6 +11,26 @@ class MedicineSchedule(Gtk.Window):
         self.vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=10)
         self.add(self.vbox)
 
+        # Labels row at the top
+        label_box = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=10)
+
+        name_label = Gtk.Label(label="Medicine Name")
+        name_label.set_xalign(0)  # Left-align the label
+        label_box.pack_start(name_label, True, True, 0)
+
+        time_label = Gtk.Label(label="Every X Time")
+        time_label.set_xalign(0)
+        label_box.pack_start(time_label, True, True, 0)
+
+        start_label = Gtk.Label(label="Start Date")
+        start_label.set_xalign(0)
+        label_box.pack_start(start_label, True, True, 0)
+
+        delete_label = Gtk.Label(label="")  # Empty label to align delete buttons
+        label_box.pack_start(delete_label, False, False, 0)
+
+        self.vbox.pack_start(label_box, False, False, 5)
+
         # Scrollable area
         self.scrolled_window = Gtk.ScrolledWindow()
         self.scrolled_window.set_policy(Gtk.PolicyType.AUTOMATIC, Gtk.PolicyType.AUTOMATIC)
