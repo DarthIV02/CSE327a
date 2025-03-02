90bbe9d25b884b5b7c95b2b70dfdc54463ca4768
Ivannia Gomez <82829719+DarthIV02@users.noreply.github.com>
2025-03-01 23:54:29 -0800

    Run concurrently

--- project_v1/main.c
+++ project_v1/main.c
@@ -6,17 +6,16 @@
 #include <stdio.h>
 
 int main(int argc, char **argv) {
-        pthread_t window_thread;
+    
+    pthread_t window_thread;
     pthread_create(&window_thread, NULL, start_window, NULL);
-
-    // Wait for the background thread to finish
-    pthread_join(window_thread, NULL);
 
     // Start the background task in a separate thread
     pthread_t countdown_alarm_thread;
     pthread_create(&countdown_alarm_thread, NULL, countdown_alarms, NULL);
 
-    // Wait for the background thread to finish
+    // Run concurrently
+    pthread_join(window_thread, NULL);
     pthread_join(countdown_alarm_thread, NULL);
 
     // Create GTK application
