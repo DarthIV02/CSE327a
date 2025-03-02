47facf675a23da352102a673b53206bbf310d503
Ivannia Gomez <82829719+DarthIV02@users.noreply.github.com>
2025-03-01 21:37:15 -0800

    Update window.c

--- project_v1/window.c
+++ project_v1/window.c
@@ -80,9 +80,10 @@ activate (GtkApplication* app,
   g_timeout_add_seconds(1, (GSourceFunc) update_time, label);
 
   //Configure style and visibility of alarm
-  gtk_widget_hide(alarm);
   gtk_widget_set_name(alarm, "alarm-label");
   apply_css(alarm);
+  gtk_widget_hide(alarm);
+  g_timeout_add_seconds(3, (GSourceFunc)gtk_widget_show, alarm);
 
   // Create buttons
   GtkWidget *patient_button = gtk_button_new_with_label ("Patient Details");
