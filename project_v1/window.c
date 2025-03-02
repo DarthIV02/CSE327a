587b179185876067d93543578e4686ba276cd59b
Ivannia Gomez <82829719+DarthIV02@users.noreply.github.com>
2025-03-01 23:20:48 -0800

    Update window.c

--- project_v1/window.c
+++ project_v1/window.c
@@ -56,7 +56,7 @@ static void schedule_clicked(GtkButton *button, gpointer user_data) {
   system("./schedule.sh");
 }
 
-static void change_alarm(int alarm_active){
+void change_alarm(int alarm_active){
   GtkWidget *alarm_here = GTK_WIDGET(alarm_pointer);
   if (alarm_active == 1){
     gtk_widget_set_name(alarm_here, "alarm-label");
