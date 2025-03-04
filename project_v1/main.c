3c8f71781bd2cb46bcfa711eb51f7052115aadd3
Ivannia Gomez <82829719+DarthIV02@users.noreply.github.com>
2025-03-03 17:15:48 -0800

    Add threads

--- project_v1/main.c
+++ project_v1/main.c
@@ -6,9 +6,31 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <semaphore.h>
+#include <wiringPi.h>
+#include <softPwm.h>
 
 int main(int argc, char **argv) {
     
+    // Initialize shared variable
+	SharedVariable v;
+
+	// Initialize WiringPi library
+	if (wiringPiSetup() == -1) {
+		printf("Failed to setup wiringPi.\n");
+		return 1; 
+	}
+
+	// Initialize shared variable and sensors
+	init_shared_variable(&v);
+	init_sensors(&v);
+
+	// Thread identifiers
+	pthread_t t_motion,
+			  t_container;
+
+    thread_create(motion);
+    thread_create(container);
+
     // Create GTK application
     pthread_t window_thread;
     pthread_create(&window_thread, NULL, start_window, NULL);
@@ -20,6 +42,9 @@ int main(int argc, char **argv) {
     // Run concurrently
     pthread_join(window_thread, NULL);
     pthread_join(countdown_alarm_thread, NULL);
+    // Wait for all threads to finish
+    thread_join(motion);
+    thread_join(container);
 
     return 1;
 }
