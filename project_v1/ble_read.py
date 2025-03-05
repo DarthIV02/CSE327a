c1442642cfd9891ee9dbb5519a8225c37a6d31e3
Ivannia Gomez <82829719+DarthIV02@users.noreply.github.com>
2025-03-05 01:14:01 -0800

    Only call once

--- project_v1/ble_read.py
+++ project_v1/ble_read.py
@@ -14,12 +14,14 @@ AUTH_TOKEN = None
 async def main():
     async with BleakClient(DEVICE) as client:
         dev_value = await client.read_gatt_char(CHARACTERISTIC)
-        called = int(dev_value[0])
-        print("Device Value: {0}".format(called))
-        if called == 1:
+        input = int(dev_value[0])
+        print("Device Value: {0}".format(input))
+        if input == 1 and called == 0:
             CARETAKER_NUMBER, ORGINATE_NUMBER, PATIENT, HELP_TEXT = get_info()
             call_for_help(ACCOUNT_SID, AUTH_TOKEN, CARETAKER_NUMBER, HELP_TEXT, ORGINATE_NUMBER)
-
+            called = 1
+        if input == 0 and called == 1:
+            called = 0
 
 if len(sys.argv) < 2:
     print("Usage: {} <addr>".format(sys.argv[0]))
