25f2feaed98342d18d5871c70233f086405948c6
Ivannia Gomez <82829719+DarthIV02@users.noreply.github.com>
2025-03-04 23:23:48 -0800

    Modify read and write ble

--- project_v1/environment.sh
+++ project_v1/environment.sh
@@ -4,3 +4,4 @@ sudo apt-get install libgtk-3-dev
 sudo apt install libcjson1 libcjson-dev
 sudo apt install -y bluetooth bluez python3-pip
 sudo apt install -y bluetooth bluez libbluetooth-dev
+sudo apt install -y python3-bleak
