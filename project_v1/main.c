c00fcef5b1d59b9fadebc03fba1cc75e381f31b4
Ivannia Gomez <82829719+DarthIV02@users.noreply.github.com>
2025-03-05 14:29:42 -0800

    Update main.c

--- project_v1/main.c
+++ project_v1/main.c
@@ -84,9 +84,9 @@ int main(int argc, char **argv) {
             if ((window_opened == 0 && val == LOW) || (window_opened == 0 && window_changed == 1)){ // Only do it when the window is not running
                 pthread_create(&window_thread, NULL, start_window, NULL);
                 window_opened = 1;
-                sem_wait(&low_priority_sem);
+                /*sem_wait(&low_priority_sem);
                 last_dt = get_time_from_hwclock();
-                sem_post(&low_priority_sem);
+                sem_post(&low_priority_sem);*/
 
             } else if(window_opened == 1 && window_changed == 0){
                 /*sem_wait(&low_priority_sem);
