a817f2caf77dfd1e4a96d886962721872e254ecf
Ivannia Gomez <82829719+DarthIV02@users.noreply.github.com>
2025-03-05 12:38:51 -0800

    Update trigger_alarms.h

--- project_v1/trigger_alarms.h
+++ project_v1/trigger_alarms.h
@@ -4,8 +4,8 @@
 
 void* countdown_alarms();
 struct tm get_time_from_hwclock();
-extern sem_t high_priority_sem, low_priority_sem;
-extern pthread_mutex_t lock;
+sem_t high_priority_sem, low_priority_sem;
+pthread_mutex_t lock;
 int high_priority_waiting;
 
 
