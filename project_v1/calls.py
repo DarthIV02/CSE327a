8d11b1bf3cade600aec44dc82b358d0c73f7887a
Ivannia Gomez <82829719+DarthIV02@users.noreply.github.com>
2025-03-05 01:05:20 -0800

    Update calls.py

--- project_v1/calls.py
+++ project_v1/calls.py
@@ -34,6 +34,7 @@ def get_info():
     # Find your Account SID and Auth Token at twilio.com/console
     # and set the environment variables. See http://twil.io/secure
     ACCOUNT_SID = os.environ.get('TWILIO_ACCOUNT_SID', "")
+    print("ACCOUNT_SID: ", ACCOUNT_SID)
     AUTH_TOKEN = os.environ.get('TWILIO_AUTH_TOKEN', "")
 
     return CARETAKER_NUMBER, ORGINATE_NUMBER, PATIENT, HELP_TEXT, ACCOUNT_SID, AUTH_TOKEN
