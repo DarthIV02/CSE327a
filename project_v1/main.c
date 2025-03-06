b5500a5f28e58c134e62366c81943f179b863a57
Ivannia Gomez <82829719+DarthIV02@users.noreply.github.com>
2025-03-05 19:18:20 -0800

    Update main.c

--- project_v1/main.c
+++ project_v1/main.c
@@ -73,6 +73,7 @@ int main(int argc, char **argv) {
             // Revert CPU Governor to Default
             printf("Turning everuthing on");
             char command[128]; 
+            char governor[128] = "ondemand";
             snprintf(command, sizeof(command), "echo %s | sudo tee /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor", governor);
             system(command);
             system("vcgencmd display_power 1");  // Turns on HDMI
